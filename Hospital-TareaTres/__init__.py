185c9eab34f7a7dab651e0368185d6f0586e1578
Yang <71834343+yizco19@users.noreply.github.com>
2024-02-16 08:56:25 -0800

    Delete Hospital-TareaTres directory

--- Hospital-TareaTres/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-# Indicamos que se importe el contenido Python de la carpeta "models"
-from . import models
-from . import controllers
--- Hospital-TareaTres/models/diagnostico.py
+++
@@ -1,9 +0,0 @@
-from odoo import api, fields, models
-
-
-class Diagnostico(models.Model):
-    _name = "hospital.diagnostico"
-    _description = "Modelo de diagnóstico"
-
-    paciente_ids = fields.Many2many('hospital.paciente', string='Pacientes')
-    medico_ids = fields.Many2many('hospital.medico', string='Medicos')
