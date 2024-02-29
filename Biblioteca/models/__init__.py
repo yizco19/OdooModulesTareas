3f015c968c5a66f77182d8ff6b35fae3bfab0c37
Yang <71834343+yizco19@users.noreply.github.com>
2024-02-29 07:25:56 -0800

    Delete Biblioteca directory

--- Biblioteca/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# Aqui indicamos que se cargara el fichero "biblioteca_comic.py"
-# Si creamos mas modelos, deben importarse en este fichero
-from . import biblioteca_comic
-from . import ejemplar
-from . import socio
